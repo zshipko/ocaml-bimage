08daf4d06a85c9318f1a582a088a6c822e9b4a6e
Zach Shipko <zachshipko@gmail.com>
2020-11-08 14:16:11 -0800

    WIP

--- io/bimage_io.ml
+++ io/bimage_io.ml
@@ -27,8 +27,6 @@ let base_type_of_ty : type a b. (a, b) Type.t -> base_type = fun (module T) ->
   match T.kind with
   | Float64 -> Double
   | Float32 -> Float
-  | Int8_signed -> Int8
-  | Int16_signed -> Int16
   | Int8_unsigned -> UInt8
   | Int16_unsigned -> UInt16
   | Int32 -> Int32
@@ -46,7 +44,7 @@ external spec_base_type : spec -> base_type = "spec_base_type"
 
 external input_open: string -> input = "input_open"
 external input_get_spec: input -> spec = "input_get_spec"
-external input_read: input -> ('a, 'b) Data.t -> unit = "input_read"
+external input_read: input -> int -> ('a, 'b) Data.t -> unit = "input_read"
 
 external output_create: string -> output = "output_create"
 external output_set_spec: output -> spec -> unit = "output_open"
@@ -71,15 +69,17 @@ module Input = struct
 
   let read input image =
     try
-       Ok (input_read input (Image.data image))
+       Ok (input_read input (Image.channels image) (Image.data image))
     with Failure reason -> Error (`Msg reason)
 
-  (*let read_image input =
+  let read_image input ty (module C: COLOR) =
     let spec = spec input in
-    let (width, height, _channels) = Spec.shape spec in
-    let bt = Spec.base_type spec in
-    let image = Image.create ty color width height in
-    read input image*)
+    let (width, height, channels) = Spec.shape spec in
+    if channels > C.channels C.t then
+      Error `Invalid_color
+    else
+      let image = Image.create ty (module C) width height in
+      read input image
 end
 
 module Output = struct
@@ -90,7 +90,7 @@ module Output = struct
       Ok (output_create filename)
     with Failure reason -> Error (`Msg reason)
 
-  let spec output s =
+  let set_spec output s =
     try
       Ok (output_set_spec output s)
     with Failure reason -> Error (`Msg reason)

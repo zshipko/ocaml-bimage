73b9ec759b041fb310da6fd467818845bef6723d
Zach Shipko <zachshipko@gmail.com>
2020-12-12 10:18:51 -0800

    Add mouse/keyboard callbacks

--- display/bimage_display.ml
+++ display/bimage_display.ml
@@ -34,8 +34,7 @@ module Texture = struct
 end
 
 module Window = struct
-  type image = Image : ('a, 'b, 'c) Image.t -> image
-  type t = { texture : Texture.t; window : GLFW.window; image: image }
+  type t = { texture : Texture.t; window : GLFW.window; image: Image.any }
 
   let init () =
     GLFW.init ();
@@ -54,25 +53,68 @@ module Window = struct
     in
     let window = GLFW.createWindow ~width ~height ~title () in
     let texture = Texture.create window image in
-    let t = { window; texture; image = Image image } in
+    let t = { window; texture; image = Image.Any image } in
     update t;
     t
   and update window =
-    let Image image = window.image in
+    let Image.Any image = window.image in
     Texture.draw window.texture window.window image
+
+  let on_mouse_button f window =
+    let g _window i b keys =
+      f window i b keys
+    in
+    ignore (GLFW.setMouseButtonCallback ~window:window.window ~f:(Some g))
+
+  let mouse_position t x y =
+    let (w, h) = GLFW.getWindowSize ~window:t.window in
+    let Any image = t.image in
+    let image_width = Float.of_int image.width in
+    let image_height = Float.of_int image.height in
+    let x' = Float.max (Float.of_int w -. Float.of_int t.texture.width) 0.0 /. 2. in
+    let y' = Float.max (Float.of_int h -. Float.of_int t.texture.height) 0.0 /. 2. in
+    let x = Float.min (Float.max (x -. x') 0.0) image_width in
+    let y = Float.min (Float.max (y -. y') 0.0) image_height in
+    (x, y)
+
+  let on_mouse_move f window =
+    let g _window x y =
+      let (x, y) = mouse_position window x y in
+      f window x y
+    in
+    ignore (GLFW.setCursorPosCallback ~window:window.window ~f:(Some g))
+
+  let on_key f window =
+    let g _window =
+      f window
+    in
+    ignore (GLFW.setKeyCallback ~window:window.window ~f:(Some g))
+
+  let get_mouse_position t =
+    let (x, y) = GLFW.getCursorPos ~window:t.window in
+    mouse_position t x y
+
+  let get_key t key =
+    GLFW.getKey ~window:t.window ~key
+
+  let get_mouse_button t button =
+    GLFW.getMouseButton ~window:t.window ~button
 
   let show window =
     GLFW.showWindow ~window:window.window
 
   let hide window =
     GLFW.hideWindow ~window:window.window
+
+  let close window =
+    hide window;
+    GLFW.setWindowShouldClose ~window:window.window ~b:true
 end
 
-let show images =
-  let windows = Hashtbl.create (List.length images) in
-  List.iter (fun (k, v) ->
-      let Bimage.Image.Any v = v in
-      Hashtbl.replace windows k (Window.create k v)) images;
+let show_all windows' =
+  let windows = Hashtbl.create 8 in
+  List.iter (fun v ->
+      Hashtbl.replace windows v v) windows';
   let should_close () = Hashtbl.fold (fun k v acc ->
       let acc = acc && GLFW.windowShouldClose ~window:v.Window.window in
       let () = if acc then
@@ -80,7 +122,7 @@ let show images =
           Hashtbl.remove windows k
       in
       acc) windows true in
-  let current_window () =Hashtbl.fold (fun _ v acc ->
+  let current_window () = Hashtbl.fold (fun _ v acc ->
       match acc with
       | Some x -> Some x
       | None -> if GLFW.getWindowAttrib ~window:v.Window.window ~attribute:Focused then
@@ -94,3 +136,17 @@ let show images =
       | None -> () in
     GLFW.pollEvents ()
   done
+
+let show images =
+  let windows = List.map (fun (k, v) ->
+      let Image.Any v = v in
+      let w = Window.create k v in
+      Window.on_key (fun window key _ action _ ->
+          match action, key with
+          | GLFW.Press, GLFW.Escape -> Window.close window
+          | _ -> ()
+        ) w;
+      w
+    ) images
+  in
+  show_all windows
--- display/bimage_display.mli
+++ display/bimage_display.mli
@@ -22,7 +22,17 @@ module Window : sig
   val create: ?width:int -> ?height:int -> string -> ('a, 'b, 'c) Bimage.Image.t -> t
   val show: t -> unit
   val hide: t -> unit
+  val close: t -> unit
   val update: t -> unit
+
+  val on_mouse_button: (t -> int -> bool -> GLFW.key_mod list -> unit) -> t -> unit
+  val on_mouse_move : (t -> float -> float -> unit) -> t -> unit
+  val on_key : (t ->
+                GLFW.key -> int -> GLFW.key_action -> GLFW.key_mod list -> unit) -> t -> unit
+  val get_mouse_position : t -> float * float
+  val get_key : t -> GLFW.key -> bool
+  val get_mouse_button : t -> int -> bool
 end
 
+val show_all: Window.t list -> unit
 val show: (string * Bimage.Image.any) list -> unit

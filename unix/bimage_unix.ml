e7fe4a7f5f3d974fea4b6086520795f16047e397
Zach Shipko <zachshipko@gmail.com>
2020-12-16 20:40:19 -0800

    Update Bimage_unix.Thread.Filter.wait

--- unix/bimage_unix.ml
+++ unix/bimage_unix.ml
@@ -8,7 +8,7 @@ module Thread = struct
     include Bimage.Filter.Make (struct
       type 'a io = 'a
 
-      let threads = Hashtbl.create 12
+      let threads = Hashtbl.create 16
 
       let bind x (f : unit -> unit io) =
         let x = f x in
@@ -20,7 +20,12 @@ module Thread = struct
 
       let wrap f = f ()
 
-      let wait () = Hashtbl.iter (fun _ v -> Thread.join v) threads
+      let wait () =
+        Hashtbl.iter
+          (fun k v ->
+            let () = Thread.join v in
+            Hashtbl.remove threads k)
+          threads
     end)
   end
 end

40b4b115ccebb1819a70b4a69cb48297971e30ae
zach <zachshipko@gmail.com>
2021-12-16 17:47:03 -0800

    Close fd in mmap function

--- unix/data.ml
+++ unix/data.ml
@@ -9,6 +9,7 @@ let mmap (type a b) ?(offset = 0L) ?(mode = 0o0655)
     in
     let arr = Bigarray.array1_of_genarray arr in
     Array1.fill arr (Bimage.Type.of_float (module T) 0.0);
+    Unix.close fd;
     arr
   in
   let arr =
@@ -16,10 +17,14 @@ let mmap (type a b) ?(offset = 0L) ?(mode = 0o0655)
       let stat = Unix.stat filename in
       let d = Bimage.Type.depth (module T) / 8 in
       let len = Int64.to_int offset + (d * n) in
-      if stat.Unix.st_size >= len then
+      if stat.Unix.st_size >= len then (
         let fd = Unix.openfile filename Unix.[ O_RDWR ] mode in
-        Unix.map_file ~pos:offset fd T.kind Bigarray.C_layout true [| n |]
-        |> Bigarray.array1_of_genarray
+        let ba =
+          Unix.map_file ~pos:offset fd T.kind Bigarray.C_layout true [| n |]
+          |> Bigarray.array1_of_genarray
+        in
+        Unix.close fd;
+        ba)
       else make_new ()
     else make_new ()
   in
